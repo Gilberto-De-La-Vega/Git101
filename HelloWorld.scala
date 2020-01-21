8d01fe59cf81ed79604a716d7e7264a9b768ad16
Gilberto <gilberto.dlv@gmail.com>
2020-01-21 15:16:16 -0800

    Your message about the commit

---
+++ HelloWorld.scala
@@ -0,0 +1,13 @@
+package com.spark_with_scala
+
+object HelloWorld extends App {
+  println("Hello World from Scala!")
+  val hello = "Hello There"
+  println(hello);
+}
+
+object HelloWorld2 extends App {
+  println("second object over here")
+  val wassup = "wassup bruh"
+  println(wassup)
+}
